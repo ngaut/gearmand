b5391b6dc9b3a2793805afe4aae04537a7ba2f10
unknown <ngaut@126.com>
2014-04-18 18:57:50 +0800

    add missing files

---
+++ server/restapi.go
@@ -0,0 +1,60 @@
+package server
+
+import (
+	"github.com/go-martini/martini"
+	"github.com/ngaut/stats"
+	"net/http/pprof"
+	_ "net/http/pprof"
+)
+
+func getJob(s *Server, params martini.Params) string {
+	e := &event{tp: ctrlGetJob,
+		jobHandle: params["jobhandle"], result: createResCh()}
+	s.ctrlEvtCh <- e
+	res := <-e.result
+
+	return res.(string)
+}
+
+func getWorker(s *Server, params martini.Params) string {
+	e := &event{tp: ctrlGetWorker,
+		args: &Tuple{t0: params["cando"]}, result: createResCh()}
+	s.ctrlEvtCh <- e
+	res := <-e.result
+
+	return res.(string)
+}
+
+func registerWebHandler(s *Server) {
+	m := martini.Classic()
+
+	m.Get("/debug/pprof", pprof.Index)
+	m.Get("/debug/pprof/cmdline", pprof.Cmdline)
+	m.Get("/debug/pprof/profile", pprof.Profile)
+	m.Get("/debug/pprof/symbol", pprof.Symbol)
+	m.Post("/debug/pprof/symbol", pprof.Symbol)
+	m.Get("/debug/pprof/block", pprof.Handler("block").ServeHTTP)
+	m.Get("/debug/pprof/heap", pprof.Handler("heap").ServeHTTP)
+	m.Get("/debug/pprof/goroutine", pprof.Handler("goroutine").ServeHTTP)
+	m.Get("/debug/pprof/threadcreate", pprof.Handler("threadcreate").ServeHTTP)
+	m.Get("/debug/stats", stats.ExpvarHandler)
+
+	m.Get("/job", func(params martini.Params) string {
+		return getJob(s, params)
+	})
+
+	//get job information using job handle
+	m.Get("/job/:jobhandle", func(params martini.Params) string {
+		return getJob(s, params)
+	})
+
+	m.Get("/worker", func(params martini.Params) string {
+		return getWorker(s, params)
+	})
+
+	m.Get("/worker/:cando", func(params martini.Params) string {
+		return getWorker(s, params)
+	})
+
+	m.Run()
+}
